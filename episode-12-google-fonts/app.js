d238d4b76be41ff1455b68d6539ebe647a4a4474
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-08-05 14:12:50 +0100

    Episdoe 12

---
+++ episode-12-google-fonts/app.js
@@ -0,0 +1,38 @@
+let form = document.querySelector("form")
+let input = document.querySelector("input")
+let terminalHistory = document.querySelector("#history")
+
+function createInputLine(command) {
+  let inputLine = document.createElement("div")
+  inputLine.className = "input-line"
+
+  let promptSpan = document.createElement("span")
+  promptSpan.className = "prompt"
+  promptSpan.append("$")
+  let inputSpan = document.createElement("span")
+  inputSpan.className = "input"
+  inputSpan.append(command)
+
+  inputLine.append(promptSpan)
+  inputLine.append(inputSpan)
+
+  return inputLine
+}
+
+function createTerminalHistoryEntry(command, commandOutput) {
+  let inputLine = createInputLine(command)
+  let output = document.createElement("div")
+  output.className = "output"
+  output.append(commandOutput)
+  terminalHistory.append(inputLine)
+  terminalHistory.append(output)
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let command = input.value
+  let output = window.api.runCommand(command)
+  createTerminalHistoryEntry(command, output)
+  input.value = ""
+  input.scrollIntoView()
+})
---
+++ episode-12-google-fonts/index.js
@@ -0,0 +1,17 @@
+let { app, BrowserWindow } = require("electron")
+
+function createWindow() {
+  let win = new BrowserWindow({
+    webPreferences: {
+      preload: `${__dirname}/preload.js`,
+    },
+  })
+  win.maximize()
+  win.loadFile("index.html")
+}
+
+app.on("ready", createWindow)
+
+app.on("window-all-closed", () => {
+  app.quit()
+})
