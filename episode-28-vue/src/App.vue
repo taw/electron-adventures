e8b3b7e89be86913b99588bd93689ce96a673cae
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-08-23 03:04:56 +0100

    Episode 28

---
+++ episode-28-vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Welcome to Vue + Electron!</h1>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style global>
+body {
+  background-color: #444;
+  color: #fff;
+}
+</style>
