f4e8730c74a40b5ee5539f9aac1a73463643c418
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-09-01 06:30:42 +0100

    Episode 39

---
+++ episode-39-keyboard-shortcut-modifier-keys/src/App.svelte
@@ -0,0 +1,86 @@
+<script>
+  import { writable } from "svelte/store"
+  import { setContext } from "svelte"
+  import Panel from "./Panel.svelte"
+  import Footer from "./Footer.svelte"
+  import EventBus from "./EventBus.js"
+  import Keyboard from "./Keyboard.svelte"
+  import CommandPalette from "./CommandPalette.svelte"
+
+  let eventBus = new EventBus()
+  let activePanel = writable("left")
+  let paletteOpen = false
+
+  setContext("app", {eventBus, activePanel})
+
+  let initialDirectoryLeft = window.api.currentDirectory()
+  let initialDirectoryRight = window.api.currentDirectory() + "/node_modules"
+
+  function switchPanel() {
+    if ($activePanel === "left") {
+      activePanel.set("right")
+    } else {
+      activePanel.set("left")
+    }
+  }
+  function activatePanel(panel) {
+    activePanel.set(panel)
+  }
+  function quit() {
+    window.close()
+  }
+  function emitToActivePanel(...args) {
+    eventBus.emit($activePanel, ...args)
+  }
+  function openPalette() {
+    paletteOpen = true
+  }
+  function closePalette() {
+    paletteOpen = false
+  }
+  eventBus.handle("app", {switchPanel, activatePanel, quit, openPalette, closePalette})
+  eventBus.handle("activePanel", {"*": emitToActivePanel})
+</script>
+
+<div class="ui">
+  <header>
+    File Manager
+  </header>
+  <Panel initialDirectory={initialDirectoryLeft} id="left" />
+  <Panel initialDirectory={initialDirectoryRight} id="right" />
+  <Footer />
+</div>
+
+<Keyboard active={!paletteOpen} />
+
+{#if paletteOpen}
+  <CommandPalette />
+{/if}
+
+<style>
+  :global(body) {
+    background-color: #226;
+    color: #fff;
+    font-family: monospace;
+    margin: 0;
+    font-size: 16px;
+  }
+  .ui {
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-areas:
+      "header header"
+      "panel-left panel-right"
+      "footer footer";
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto minmax(0, 1fr) auto;
+  }
+  .ui header {
+    grid-area: header;
+  }
+  header {
+    font-size: 24px;
+    margin: 4px;
+  }
+</style>
---
+++ episode-39-keyboard-shortcut-modifier-keys/src/CommandPaletteEntry.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Shortcut from "./Shortcut.svelte"
+  import { getContext } from "svelte"
+  let { eventBus } = getContext("app")
+
+  export let name
+  export let match = undefined
+  export let shortcuts = []
+  export let action
+
+  function handleClick() {
+    eventBus.emit("app", "closePalette")
+    eventBus.emit(...action)
+  }
+</script>
+
+<li on:click={handleClick}>
+  <span class="name">
+    {#if match}
+      {#each match as [part, highlight]}
+        {#if highlight}
+          <em>{part}</em>
+        {:else}
+          {part}
+        {/if}
+      {/each}
+    {:else}
+      {name}
+    {/if}
+  </span>
+  {#each shortcuts as shortcut}
+    <Shortcut {...shortcut} />
+  {/each}
+</li>
+
+<style>
+  li {
+    display: flex;
+    padding: 0px 8px;
+  }
+  li:first-child {
+    background-color: #66b;
+  }
+  .name {
+    flex: 1;
+  }
+  .name em {
+    color: #ff2;
+    font-weight: bold;
+    font-style: normal;
+  }
+</style>
