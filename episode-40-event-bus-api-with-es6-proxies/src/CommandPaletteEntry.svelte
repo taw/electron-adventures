8d44e6a49cd1c0bc0cde5c45ac5eaef9a1496082
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-09-01 07:30:55 +0100

    Episode 40

---
+++ episode-40-event-bus-api-with-es6-proxies/src/CommandPaletteEntry.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Shortcut from "./Shortcut.svelte"
+  import { getContext } from "svelte"
+  let { eventBus } = getContext("app")
+  let app = eventBus.target("app")
+
+  export let name
+  export let match = undefined
+  export let shortcuts = []
+  export let action
+
+  function handleClick() {
+    app.closePalette()
+    eventBus.emit(...action)
+  }
+</script>
+
+<li on:click={handleClick}>
+  <span class="name">
+    {#if match}
+      {#each match as [part, highlight]}
+        {#if highlight}
+          <em>{part}</em>
+        {:else}
+          {part}
+        {/if}
+      {/each}
+    {:else}
+      {name}
+    {/if}
+  </span>
+  {#each shortcuts as shortcut}
+    <Shortcut {...shortcut} />
+  {/each}
+</li>
+
+<style>
+  li {
+    display: flex;
+    padding: 0px 8px;
+  }
+  li:first-child {
+    background-color: #66b;
+  }
+  .name {
+    flex: 1;
+  }
+  .name em {
+    color: #ff2;
+    font-weight: bold;
+    font-style: normal;
+  }
+</style>
