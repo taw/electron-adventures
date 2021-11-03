cd143384de32525da4fa7681aa4ea9342f9c0f0a
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-11-03 18:13:49 +0000

    Episode 86

---
+++ episode-86-remembering-user-preferences/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import RouletteWheel from "./RouletteWheel.svelte"
+
+  function onRolled(opt) {
+    console.log(opt)
+  }
+</script>
+
+<div>
+  <svg height="400" width="400">
+    <g transform="translate(200,200)">
+      <RouletteWheel
+        r={150}
+        options={[1,2,3,4,5,6,7,8,9,10,11,12]}
+        onRolled={onRolled}
+      />
+    </g>
+    <polygon points="200 360 210 370 190 370"/>
+  </svg>
+</div>
+
+<style>
+:global(body) {
+  background-color: #444;
+  color: #fff;
+  margin: 0;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  user-select: none;
+}
+
+svg {
+  display: block;
+}
+
+polygon {
+  fill: yellow;
+}
+</style>
---
+++ episode-86-remembering-user-preferences/src/ArcLabel.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let cx=0, cy=0, r, a, text
+
+  let x = cx + Math.sin(a * 2 * Math.PI / 360.0) * r
+  let y = cy + Math.cos(a * 2 * Math.PI / 360.0) * r
+</script>
+
+<g transform={`translate(${x},${y}) rotate(${180-a})`}>
+  <text x={0} y={0} text-anchor="middle">{text}</text>
+</g>
+
+<style>
+  text {
+    font-size: 24px;
+    font-family: sans-serif;
+    fill: red;
+  }
+</style>
