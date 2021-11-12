0c44a0bc43e492200d289383f7ac7e912523ebeb
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-11-12 15:06:17 +0000

    Episdoe 96

---
+++ episode-96-pywebview-terminal-app/terminal.js
@@ -0,0 +1,38 @@
+let form = document.querySelector("form")
+let input = document.querySelector("input")
+let terminalHistory = document.querySelector("#history")
+
+function createInputLine(command) {
+  let inputLine = document.createElement("div")
+  inputLine.className = "input-line"
+
+  let promptSpan = document.createElement("span")
+  promptSpan.className = "prompt"
+  promptSpan.append("$")
+  let inputSpan = document.createElement("span")
+  inputSpan.className = "input"
+  inputSpan.append(command)
+
+  inputLine.append(promptSpan)
+  inputLine.append(inputSpan)
+
+  return inputLine
+}
+
+function createTerminalHistoryEntry(command, commandOutput) {
+  let inputLine = createInputLine(command)
+  let output = document.createElement("div")
+  output.className = "output"
+  output.append(commandOutput)
+  terminalHistory.append(inputLine)
+  terminalHistory.append(output)
+}
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault()
+  let command = input.value
+  let output = await window.pywebview.api.execute(command)
+  createTerminalHistoryEntry(command, output)
+  input.value = ""
+  input.scrollIntoView()
+})
